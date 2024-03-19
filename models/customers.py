b80a5a649d03a19aae512eed05939f76f1bfa269
Brian <brianmariga0@gmail.com>
2024-03-19 17:48:33 +0300

    Initialization

---
+++ models/customers.py
@@ -0,0 +1,26 @@
+from __init__ import CURSOR, CONN
+
+class Customer:
+
+    def __init__(self, id, first_name, last_name):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def reviews(self):
+        pass
+
+    def restaurant(self):
+        pass
+
+    def full_name(self):
+        pass
+
+    def favorite_restaurant(self):
+        pass
+
+    def add_review(self):
+        pass
+
+    def delete_reviews(self):
+        pass
---
+++ models/reviews.py
@@ -0,0 +1,16 @@
+from __init__ import CURSOR, CONN
+
+class Review:
+
+    def __init__(self, id, star_rating):
+        self.id = id
+        self.star_rating = star_rating
+
+    def customer(self):
+        pass
+
+    def restaurant(self):
+        pass
+
+    def full_review(self):
+        pass
